7167d8d8d246d70607deb24d07ffea5f1d1d914b
PabloAndresCQ <pablo.andres-martinez@cambridgequantum.com>
2023-04-24 15:31:29 +0200

    Added and tested some utility functions.

--- tests/test_mps.py
+++ tests/test_mps.py
@@ -83,9 +83,17 @@ def test_canonicalise():
                 T_d[i0][i1] = np.random.rand() + 1j*np.random.rand()
         mps.tensors[len(mps)-1] = Tensor(T_d, bonds=[len(mps)-1, 2*len(mps)-1])
 
-        # Canonicalise around center_pos
-        center_pos = 2
-        mps.canonicalise(l_pos=center_pos, r_pos=center_pos)
+        with mps.copy() as mps_copy:
+            # Calculate the norm of the MPS
+            abs_sq = mps.vdot(mps_copy)
+
+            # Canonicalise around center_pos
+            center_pos = 2
+            mps.canonicalise(l_pos=center_pos, r_pos=center_pos)
+
+            # Check that canonicalisation did not change the vector
+            overlap = mps.vdot(mps_copy)
+            assert np.isclose(overlap, abs_sq)
 
         # Check that the corresponding tensors are in orthogonal form
         for pos in range(len(mps)):
@@ -217,4 +225,48 @@ def test_line_circ_approx():
                 q1 = qubit_pos[g.qubits[1]]
                 mps.apply_2q_gate((q0,q1), g.op)
         assert mps.is_valid()
-        assert mps.fidelity < 1
+        assert np.isclose(mps.fidelity, 0.00013, atol=1e-6)
+
+
+def test_vdot():
+    np.random.seed(1)
+    n_qubits = 5
+    layers = 30
+
+    c = Circuit(n_qubits)
+
+    for i in range(layers):
+
+        # Layer of TK1 gates
+        for q in range(n_qubits):
+            c.TK1(np.random.rand(), np.random.rand(), np.random.rand(), q)
+
+        # Layer of CX gates
+        offset = np.mod(i, 2)  # Even layers connect (q0,q1), odd (q1,q2)
+        qubit_pairs = [
+            [c.qubits[i], c.qubits[i + 1]]
+            for i in range(offset, n_qubits - 1, 2)
+        ]
+        # Direction of each CX gate is random
+        for pair in qubit_pairs:
+            np.random.shuffle(pair)
+
+        for pair in qubit_pairs:
+            c.CX(pair[0], pair[1])
+
+    qubit_pos = {q : i for i, q in enumerate(c.qubits)}
+
+    with MPSxGate(n_tensors=n_qubits, chi=4) as mps:
+        # Apply each of the gates
+        for g in c.get_commands():
+            if len(g.qubits) == 1:
+                q = g.qubits[0]
+                mps.apply_1q_gate(qubit_pos[q], g.op)
+            else:
+                q0 = qubit_pos[g.qubits[0]]
+                q1 = qubit_pos[g.qubits[1]]
+                mps.apply_2q_gate((q0,q1), g.op)
+        assert mps.is_valid()
+
+        with mps.copy() as mps_copy:
+            assert np.isclose(mps.vdot(mps_copy), 1.0)
