4e4f252ca403039bf7ad4058f20762ced1ab2a5b
Iakov Polyak <yapolyak@gmail.com>
2023-05-22 09:18:27 -0700

    Add a new converter unit test.

--- tests/test_tensor_network_convert.py
+++ tests/test_tensor_network_convert.py
@@ -1,8 +1,13 @@
 from typing import List, Union
 import warnings
+import cmath
 import numpy as np
 from numpy.typing import NDArray
+import random
 import pytest
+from pytket.circuit import Circuit, ToffoliBox, Qubit
+from pytket.passes import DecomposeBoxes, CnXPairwiseDecomposition
+from pytket.transform import Transform
 
 try:
     import cuquantum as cq  # type: ignore
@@ -53,3 +58,45 @@ def test_convert_statevec_overlap(circuit: Circuit) -> None:
     assert np.allclose(result_cu, state_vector)
     ovl = circuit_overlap_contract(circuit)
     assert ovl == pytest.approx(1.0)
+
+
+@pytest.mark.parametrize("n_qubits", [2])
+def test_toffoli_box_with_implicit_swaps(n_qubits) -> None:
+
+    def to_bool_tuple(n_qubits, x):
+        bool_list = []
+        for i in reversed(range(n_qubits)):
+            bool_list.append((x >> i) % 2 == 1)
+        return tuple(bool_list)
+    
+    # Generate a random permutation
+    random.seed(1)
+    cycle = list(range(2**n_qubits))
+    random.shuffle(cycle)
+    
+    perm = dict()
+    for orig, dest in enumerate(cycle):
+        perm[to_bool_tuple(n_qubits, orig)] = to_bool_tuple(n_qubits, dest)
+    
+    # Create a circuit with more qubits and multiple applications of the permutation above
+    
+    ket_circ = Circuit(3)
+    
+    # Create the circuit
+    ket_circ.add_toffolibox(ToffoliBox(perm), [Qubit(0), Qubit(1)])
+    ket_circ.add_toffolibox(ToffoliBox(perm), [Qubit(1), Qubit(2)])
+    
+    DecomposeBoxes().apply(ket_circ)
+    CnXPairwiseDecomposition().apply(ket_circ)
+    Transform.OptimiseCliffords().apply(ket_circ)
+    
+    # Convert and contract
+    ket_net = TensorNetwork(ket_circ)
+    ket_net_vector = cq.contract(*ket_net.cuquantum_interleaved).flatten()
+    ket_net_vector = ket_net_vector * cmath.exp(1j*cmath.pi*ket_circ.phase)
+    
+    # Compare to pytket statevector
+    ket_pytket_vector = ket_circ.get_statevector()
+    
+    assert np.allclose(ket_net_vector, ket_pytket_vector)
+
