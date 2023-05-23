4804a62232608c2dafa977514fede62244ca746f
Iakov Polyak <45295461+yapolyak@users.noreply.github.com>
2023-05-23 12:26:29 +0100

    Second attempt to get qubit order right in the converter, taking implicit swaps into account. (#6)
    * Order TN output edge indices according to `Graph(circuit).output_names` qubit names rather than the `input_names` ones.
    
    * black / lint
    
    * Remove qubit permutation from backend  as we now deal with it in the converter.
    
    * Add a new converter unit test.
    
    * black / lint
    
    * Satisfied mypy.
    
    * Cleaned-up the unit test.
    
    * Added a general Toffoli box test.
    
    * black
    
    * mypy

--- tests/test_tensor_network_convert.py
+++ tests/test_tensor_network_convert.py
@@ -1,8 +1,13 @@
 from typing import List, Union
 import warnings
+import random
+import cmath
 import numpy as np
 from numpy.typing import NDArray
 import pytest
+from pytket.circuit import ToffoliBox, Qubit  # type: ignore
+from pytket.passes import DecomposeBoxes, CnXPairwiseDecomposition  # type: ignore
+from pytket.transform import Transform  # type: ignore
 
 try:
     import cuquantum as cq  # type: ignore
@@ -53,3 +58,85 @@ def test_convert_statevec_overlap(circuit: Circuit) -> None:
     assert np.allclose(result_cu, state_vector)
     ovl = circuit_overlap_contract(circuit)
     assert ovl == pytest.approx(1.0)
+
+
+def test_toffoli_box_with_implicit_swaps() -> None:
+    # Using specific permutation here
+    perm = {
+        (False, False): (True, True),
+        (False, True): (False, False),
+        (True, False): (True, False),
+        (True, True): (False, True),
+    }
+
+    # Create a circuit with more qubits and multiple applications of the permutation
+    # above
+    ket_circ = Circuit(3)
+
+    # Create the circuit
+    ket_circ.add_toffolibox(ToffoliBox(perm), [Qubit(0), Qubit(1)])
+    ket_circ.add_toffolibox(ToffoliBox(perm), [Qubit(1), Qubit(2)])
+
+    DecomposeBoxes().apply(ket_circ)
+    CnXPairwiseDecomposition().apply(ket_circ)
+    Transform.OptimiseCliffords().apply(ket_circ)
+
+    # Convert and contract
+    ket_net = TensorNetwork(ket_circ)
+    ket_net_vector = cq.contract(*ket_net.cuquantum_interleaved).flatten()
+    ket_net_vector = ket_net_vector * cmath.exp(1j * cmath.pi * ket_circ.phase)
+
+    # Compare to pytket statevector
+    ket_pytket_vector = ket_circ.get_statevector()
+
+    assert np.allclose(ket_net_vector, ket_pytket_vector)
+
+
+@pytest.mark.parametrize("n_qubits", [4, 5, 6])
+def test_generalised_toffoli_box(n_qubits: int) -> None:
+    def to_bool_tuple(n_qubits: int, x: int) -> tuple:
+        bool_list = []
+        for i in reversed(range(n_qubits)):
+            bool_list.append((x >> i) % 2 == 1)
+        return tuple(bool_list)
+
+    random.seed(1)
+
+    # Generate a random permutation
+    cycle = list(range(2**n_qubits))
+    random.shuffle(cycle)
+
+    perm = dict()
+    for orig, dest in enumerate(cycle):
+        perm[to_bool_tuple(n_qubits, orig)] = to_bool_tuple(n_qubits, dest)
+
+    # Create a circuit implementing the permutation above
+    ket_circ = ToffoliBox(perm).get_circuit()
+
+    DecomposeBoxes().apply(ket_circ)
+    CnXPairwiseDecomposition().apply(ket_circ)
+    Transform.OptimiseCliffords().apply(ket_circ)
+
+    # The ideal outcome on ket 0 input
+    output = perm[(False,) * n_qubits]
+    # A trivial circuit generating this state
+    bra_circ = Circuit()
+    for q in ket_circ.qubits:
+        bra_circ.add_qubit(q)
+    for i, bit in enumerate(output):
+        if bit:
+            bra_circ.X(i)
+
+    ket_net = TensorNetwork(ket_circ)
+    ket_net_vector = cq.contract(*ket_net.cuquantum_interleaved).flatten()
+    ket_net_vector = ket_net_vector * cmath.exp(1j * cmath.pi * ket_circ.phase)
+    ket_pytket_vector = ket_circ.get_statevector()
+    assert np.allclose(ket_net_vector, ket_pytket_vector)
+
+    bra_net = TensorNetwork(bra_circ)
+    bra_net_vector = cq.contract(*bra_net.cuquantum_interleaved).flatten()
+    bra_net_vector = bra_net_vector * cmath.exp(1j * cmath.pi * bra_circ.phase)
+    bra_pytket_vector = bra_circ.get_statevector()
+    assert np.allclose(bra_net_vector, bra_pytket_vector)
+
+    np.isclose(abs(cq.contract(*ket_net.vdot(bra_net))), 1.0)
