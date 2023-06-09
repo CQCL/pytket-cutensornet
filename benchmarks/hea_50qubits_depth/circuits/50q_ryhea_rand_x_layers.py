14c3216304b485761a8aa9960e7305187722807a
Nathan Fitzpatrick <45206408+NathanCQC@users.noreply.github.com>
2023-06-09 01:04:57 -0700

    Pulled in benchmarks

--- benchmarks/hea_50qubits_depth/circuits/50q_ryhea_rand_x_layers.py
+++ benchmarks/hea_50qubits_depth/circuits/50q_ryhea_rand_x_layers.py
@@ -1,12 +1,14 @@
 import pickle
 from pathlib import Path
 from pytket.circuit import Circuit
+import sys
+ 
+n_layers = int(sys.argv[1])
 
 path = Path("./hea_50qubits_depth")
 path.mkdir(parents=True, exist_ok=True)
 
 n_qubits = 50
-n_layers = 6
 
 with open(path / f'hea_ry_{n_qubits}_q_{n_layers}_layers_rand.pkl','rb')as file:
     circuit = Circuit.from_dict(pickle.load(file))
