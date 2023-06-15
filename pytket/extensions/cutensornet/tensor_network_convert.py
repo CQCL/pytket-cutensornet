162d738003fac939640aab4807a27d138616777d
Nathan Fitzpatrick <45206408+NathanCQC@users.noreply.github.com>
2023-06-15 14:46:50 +0100

    fixed rebase

--- pytket/extensions/cutensornet/tensor_network_convert.py
+++ pytket/extensions/cutensornet/tensor_network_convert.py
@@ -544,57 +544,6 @@ def measure_qubits_state(
 
 
 class PauliOperatorTensorNetwork:
-    """Handles a tensor network representing a Pauli operator string."""
-
-    Args:
-        ket: a TensorNetwork object representing a quantum state.
-        qubit_id: a qubit id.
-        bit_value: a bit value to be assigned to the measured qubit.
-        loglevel: logging level.
-
-    Returns:
-        A TensorNetwork object representing a quantum state after the 
-        measurement with a modified interleaved notation containing the extra 
-        measurement tensor.
-    """
-
-    cap = {
-        0: np.array([1, 0], dtype="complex128"),
-        1: np.array([0, 1], dtype="complex128"),
-    }
-
-    sticky_ind = ket.sticky_indices[qubit_id]
-    ket._cuquantum_interleaved.extend([cap[bit_value], [sticky_ind]])
-    return ket
-
-
-# TODO: Make this compatible with mid circuit measurements and reset
-def measure_qubits_state(
-    ket: TensorNetwork, measurement_dict: dict[Qubit, int], loglevel: int = logging.INFO
-) -> TensorNetwork:
-    """Measures a list of qubits in a tensor network. by appending a
-    measurement gate to the tensor network.
-    The measurment gate is applied via appending a tensor cap
-    of the form:  0: [1, 0] or 1: [0, 1] to the interleaved einsum input.
-    Therefor removing the open indices of the tensor network corresponding
-      to the measured qubits.
-
-    Args:
-        ket: a TensorNetwork object representing a quantum state.
-        measurement_dict: a dictionary of qubit ids and their
-        corresponding bit values to be assigned to the measured qubits.
-
-    Returns:
-        A TensorNetwork object representing a quantum state after
-        the measurement with a modified interleaved notation containing
-        the extra measurement tensors.
-    """
-    for qubit_id, bit_value in measurement_dict.items():
-        ket = _measure_qubit_state(ket, qubit_id, bit_value)
-    return ket
-
-
-class PauliOperatorTensorNetwork:
     PAULI = {
         "X": np.array([[0, 1], [1, 0]], dtype="complex128"),
         "Y": np.array([[0, -1j], [1j, 0]], dtype="complex128"),
