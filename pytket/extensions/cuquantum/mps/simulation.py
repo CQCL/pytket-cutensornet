c0d0a027228f0212aca91b8ab4a2b206ab030d82
PabloAndresCQ <pablo.andres-martinez@cambridgequantum.com>
2023-06-20 16:30:43 +0100

    The simulate method now sorts gates so that there's less overhead due to canonicalisation.

--- pytket/extensions/cuquantum/mps/simulation.py
+++ pytket/extensions/cuquantum/mps/simulation.py
@@ -1,7 +1,8 @@
 from typing import Any
 from enum import Enum
+from random import choice
 
-from pytket.circuit import Circuit  # type ignore
+from pytket.circuit import Circuit, Command  # type ignore
 from pytket.passes import DecomposeBoxes  # type: ignore
 from pytket.transform import Transform  # type: ignore
 from pytket.architecture import Architecture  # type: ignore
@@ -51,8 +52,12 @@ def simulate(
     else:
         print(f"Unrecognised algorithm: {algorithm}.")
 
+    # Sort the gates so there isn't much overhead from canonicalising back and forth.
+    sorted_gates = get_sorted_gates(prep_circ)
+
+    # Apply the gates
     with mps.init_cutensornet():
-        for g in prep_circ.get_commands():
+        for g in sorted_gates:
             mps.apply_gate(g)
 
     return mps
@@ -83,3 +88,78 @@ def prepare_circuit(circuit: Circuit) -> Circuit:
     Transform.DecomposeBRIDGE().apply(prep_circ)
 
     return prep_circ
+
+
+def get_sorted_gates(circuit: Circuit) -> [Command]:
+    """Sort the list of gates so that we obtain an equivalent circuit where we apply
+    2-qubit gates that are close to each other first. This reduces the overhead of
+    canonicalisation of the MPS, since we try to apply as many gates as we can on one
+    end of the MPS before we go to the other end.
+
+    Args:
+        circuit: The original circuit.
+
+    Returns:
+        The same gates, ordered in a beneficial way.
+    """
+    all_gates = circuit.get_commands()
+    sorted_gates = []
+    # Keep track of the qubit at the center of the canonical form; start arbitrarily
+    current_qubit = circuit.qubits[0]
+    # Entries from `all_gates` that are not yet in `sorted_gates`
+    remaining = set(range(len(all_gates)))
+
+    # Create the list of indices of gates acting on each qubit
+    gate_indices = {q: [] for q in circuit.qubits}
+    for i, g in enumerate(all_gates):
+        for q in g.qubits:
+            gate_indices[q].append(i)
+    # Apply all 1-qubit gates at the beginning of the circuit
+    for q, indices in gate_indices.items():
+        while indices and len(all_gates[indices[0]].qubits) == 1:
+            i = indices.pop(0)
+            sorted_gates.append(all_gates[i])
+            remaining.remove(i)
+    # Decide which 2-qubit gate to apply next
+    while remaining:
+        q_index = circuit.qubits.index(current_qubit)
+        # Find the distance from q_index to the first qubit with an applicable 2-qubit gate
+        left_distance = None
+        prev_q = current_qubit
+        for i, q in enumerate(reversed(circuit.qubits[:q_index])):
+            if gate_indices[prev_q] and gate_indices[q] and gate_indices[prev_q][0] == gate_indices[q][0]:
+                left_distance = i
+                break
+            prev_q = q
+        right_distance = None
+        prev_q = current_qubit
+        for i, q in enumerate(circuit.qubits[q_index+1:]):
+            if gate_indices[prev_q] and gate_indices[q] and gate_indices[prev_q][0] == gate_indices[q][0]:
+                right_distance = i
+                break
+            prev_q = q
+        # Choose the shortest distance
+        if left_distance is None:
+            current_qubit = circuit.qubits[q_index + right_distance]
+        elif right_distance is None:
+            current_qubit = circuit.qubits[q_index - left_distance]
+        elif left_distance < right_distance:
+            current_qubit = circuit.qubits[q_index - left_distance]
+        elif left_distance > right_distance:
+            current_qubit = circuit.qubits[q_index + right_distance]
+        else:
+            current_qubit = circuit.qubits[q_index + choice([-left_distance, right_distance])]
+        # Apply the gate
+        i = gate_indices[current_qubit].pop(0)
+        sorted_gates.append(all_gates[i])
+        remaining.remove(i)
+        # Apply all 1-qubit gates after this gate
+        for q in all_gates[i].qubits:
+            indices = gate_indices[q]
+            while indices and len(all_gates[indices[0]].qubits) == 1:
+                i = indices.pop(0)
+                sorted_gates.append(all_gates[i])
+                remaining.remove(i)
+
+    assert len(all_gates) == len(sorted_gates)
+    return sorted_gates
