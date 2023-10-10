47fd9038d7dc9063bccca53b0e2d3a40fce62529
PabloAndresCQ <pablo.andres-martinez@cambridgequantum.com>
2023-10-10 09:20:46 -0600

    Began implementation of TTN

---
+++ pytket/extensions/cutensornet/states/ttn.py
@@ -0,0 +1,311 @@
+# Copyright 2019-2023 Quantinuum
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+##
+#     http://www.apache.org/licenses/LICENSE-2.0
+##
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations  # type: ignore
+import warnings
+from typing import Optional, NamedTuple
+from enum import IntEnum
+
+from random import random  # type: ignore
+import math  # type: ignore
+import numpy as np  # type: ignore
+
+try:
+    import cupy as cp  # type: ignore
+except ImportError:
+    warnings.warn("local settings failed to import cupy", ImportWarning)
+try:
+    import cuquantum as cq  # type: ignore
+    from cuquantum.cutensornet import tensor  # type: ignore
+except ImportError:
+    warnings.warn("local settings failed to import cutensornet", ImportWarning)
+
+from pytket.circuit import Command, Op, OpType, Qubit
+from pytket.pauli import Pauli, QubitPauliString
+
+from pytket.extensions.cutensornet.general import set_logger
+
+from .general import CuTensorNetHandle, Tensor, Config
+
+
+class DirTTN(IntEnum):
+    """An enum to refer to relative directions within the TTN."""
+
+    PARENT = -1
+    LEFT = 0
+    RIGHT = 1
+
+
+# An alias for the TTN path from root to a TreeNode
+RootPath = tuple[DirTTN, ...]
+
+
+class TreeNode(NamedTuple):
+    """Represents a single tensor in the TTN.
+
+    The shape of the tensor agrees with the convention set in ``DirTTN``, meaning
+    that ``tensor.shape[DirTTN.PARENT]`` corresponds to the dimension of the bond
+    connecting this tree node with its parent. Notice that, since DirTTN.PARENT is
+    -1, this is always the last entry.
+
+    In the case the TreeNode is a leaf, it will contain only one virtual bond
+    (the parent) and as many physical bonds as qubits in the group it represents.
+    These qubits will correspond to bonds from ``0`` to ``len(tensor.shape)``.
+    """
+
+    tensor: Tensor
+    is_leaf: bool = False
+    canonical_form: Optional[DirTTN] = None
+
+    def copy(self) -> TreeNode:
+        return TreeNode(
+            self.tensor.copy(),
+            is_leaf=self.is_leaf,
+            canonical_form=self.canonical_form,
+        )
+
+
+class TTN:
+    """Represents a state as a Tree Tensor Network.
+
+    Attributes:
+        nodes (dict[RootPath, TreeNode]): A dictionary providing the tree node
+            of the given root path in the TTN.
+        qubit_position (dict[pytket.circuit.Qubit, tuple[RootPath, int]]): A dictionary
+            mapping circuit qubits to their address in the TTN.
+        fidelity (float): A lower bound of the fidelity, obtained by multiplying
+            the fidelities after each contraction. The fidelity of a contraction
+            corresponds to ``|<psi|phi>|^2`` where ``|psi>`` and ``|phi>`` are the
+            states before and after truncation (assuming both are normalised).
+    """
+
+    def __init__(
+        self,
+        libhandle: CuTensorNetHandle,
+        qubit_partition: dict[int, list[Qubit]],
+        config: Config,
+    ):
+        """Initialise a TTN on the computational state ``|0>``.
+
+        Note:
+            A ``libhandle`` should be created via a ``with CuTensorNet() as libhandle:``
+            statement. The device where the TTN is stored will match the one specified
+            by the library handle.
+
+            The current implementation requires the keys of ``qubit_partition`` to be
+            integers from ``0`` to ``2^l - 1`` for some ``l``. The cost of applying
+            gates between qubits on ``qubit_partition[i]`` and ``qubit_partition[j]``
+            scales exponentially on ``|i-j|``.
+
+        Args:
+            libhandle: The cuTensorNet library handle that will be used to carry out
+                tensor operations on the TTN.
+            qubits: A partition of the qubits in the circuit into disjoint groups.
+            config: The object describing the configuration for simulation.
+
+        Raises:
+            ValueError: If the keys of ``qubit_partition`` do not range from ``0`` to
+                ``2^l - 1`` for some ``l``.
+            ValueError: If a ``Qubit`` is repeated in ``qubit_partition``.
+        """
+        self._lib = libhandle
+        self._cfg = config
+        self._logger = set_logger("TTN", level=config.loglevel)
+        self.fidelity = 1.0
+        self.nodes: dict[RootPath, TreeNode] = dict()
+        self.qubit_position: dict[Qubit, tuple[RootPath, int]] = dict()
+
+        n_groups = len(qubit_partition)
+        if n_groups == 0:  # There's no initialisation to be done
+            return None
+
+        n_levels = math.floor(math.log2(n_groups))
+        if n_groups != 2**n_levels:
+            raise ValueError(
+                "The number of entries in qubit_partition must be a power of two."
+            )
+
+        # Create the TreeNodes of the different groups of qubits
+        for k, qubits in qubit_partition.items():
+            if k < 0 or k >= n_groups:
+                raise ValueError(
+                    f"The keys of qubit_partition must range from 0 to {n_groups-1}."
+                )
+
+            # Calculate the root path of this group
+            for l in reversed(range(n_levels)):
+                path = []
+                if k < 2**l:
+                    path.append(DirTTN.LEFT)
+                else:
+                    path.append(DirTTN.RIGHT)
+                    k -= 2**l
+
+            # Add each qubit to the qubit_position dictionary
+            for i, q in enumerate(qubits):
+                if q in self.qubit_position:
+                    raise ValueError(
+                        f"Qubit {q} appears in multiple entries of qubit_partition."
+                    )
+                self.qubit_position[q] = (tuple(path), i)
+
+            # This tensor has a physical bond per qubit and one virtual bond at the
+            # end for the parent (dim=1)
+            shape = tuple([2] * len(qubits) + [1])
+            # Initialise the tensor of this group of qubits to |0>
+            tensor = cp.zeros(shape=shape, dtype=self._cfg._complex_t)
+            ket_zero_entry = tuple(0 for _ in shape)  # Index 0 on all bonds
+            tensor[ket_zero_entry] = 1  # Amplitude of |0> set to 1
+
+            # Create the TreeNode
+            node = TreeNode(tensor, is_leaf=True)
+            self.nodes[tuple(path)] = node
+
+        # Create the internal TreeNodes
+        paths: list[list[DirTTN]] = [[]]
+        for _ in range(n_levels - 1):
+            # Create the TreeNode at this path
+            for p in paths:
+                tensor = cp.ones(shape=(1, 1, 1), dtype=self._cfg._complex_t)
+                self.nodes[tuple(p)] = TreeNode(tensor)
+            # Generate the paths for the next level
+            paths = [
+                p + [direction]
+                for p in paths
+                for direction in [DirTTN.LEFT, DirTTN.RIGHT]
+            ]
+
+    def is_valid(self) -> bool:
+        """Verify that the TTN object is valid.
+
+        Specifically, verify that the TTN does not exceed the dimension limit ``chi``
+        specified in the ``Config`` object, that physical bonds have dimension 2,
+        that all tensors except the leaves are rank three and that tensors have shapes
+        consistent with the bond dimensions.
+
+        Returns:
+            False if a violation was detected or True otherwise.
+        """
+        chi_ok = all(
+            self.get_dimension(path, DirTTN.PARENT) <= self._cfg.chi
+            for path in self.nodes.keys()
+        )
+        phys_ok = all(
+            self.nodes[path].tensor.shape[bond] == 2
+            for path, bond in self.qubit_position.values()
+        )
+        rank_ok = all(
+            node.is_leaf or node.tensor.shape == 3 for node in self.nodes.values()
+        )
+        shape_ok = all(
+            self.get_dimension(path, DirTTN.PARENT)
+            == self.get_dimension(path, path[-1])
+            for path in self.nodes.keys()
+            if path != []
+        )
+
+        # Debugger logging
+        self._logger.debug(
+            "Checking validity of MPS... "
+            f"chi_ok={chi_ok}, "
+            f"phys_ok={phys_ok}, "
+            f"rank_ok={rank_ok}, "
+            f"shape_ok={shape_ok}"
+        )
+        return chi_ok and phys_ok and rank_ok and shape_ok
+
+    def get_qubits(self) -> set[Qubit]:
+        """Returns the set of qubits that this TTN is defined on."""
+        return set(self.qubit_position.keys())
+
+    def get_dimension(self, path: RootPath, direction: DirTTN) -> int:
+        """Returns the dimension of bond ``dir`` of the node at ``path``.
+
+        Args:
+            path: The path to a node in the TTN.
+            direction: The direction of the bond.
+
+        Returns:
+            The dimension of the bond between the node and its parent.
+
+        Raises:
+            ValueError: If ``path`` is not in the TTN.
+        """
+        if path not in self.nodes:
+            raise ValueError(f"The path {path} is not in the TTN.")
+
+        dim: int = self.nodes[path].tensor.shape[direction]
+        return dim
+
+    def get_byte_size(self) -> int:
+        """
+        Returns:
+            The number of bytes the TTN currently occupies in GPU memory.
+        """
+        return sum(node.tensor.nbytes for node in self.nodes.values())
+
+    def get_device_id(self) -> int:
+        """
+        Returns:
+            The identifier of the device (GPU) where the tensors are stored.
+        """
+        return int(self.nodes[tuple()].tensor.device)
+
+    def update_libhandle(self, libhandle: CuTensorNetHandle) -> None:
+        """Update the ``CuTensorNetHandle`` used by this ``TTN`` object. Multiple
+        objects may use the same handle.
+
+        Args:
+            libhandle: The new cuTensorNet library handle.
+
+        Raises:
+            RuntimeError: If the device (GPU) where ``libhandle`` was initialised
+                does not match the one where the tensors of the TTN are stored.
+        """
+        if libhandle.device_id != self.get_device_id():
+            raise RuntimeError(
+                "Device of libhandle is not the one where the TTN is stored.",
+                f"{libhandle.device_id} != {self.get_device_id()}",
+            )
+        self._lib = libhandle
+
+    def copy(self) -> TTN:
+        """
+        Returns:
+            A deep copy of the TTN on the same device.
+        """
+
+        # Create a dummy object
+        new_ttn = TTN(self._lib, qubit_partition=dict(), config=self._cfg.copy())
+        # Copy all data
+        new_ttn.fidelity = self.fidelity
+        new_ttn.nodes = {path: node.copy() for path, node in self.nodes.items()}
+        new_ttn.qubit_position = self.qubit_position.copy()
+
+        self._logger.debug(
+            "Successfully copied a TTN "
+            f"of size {new_ttn.get_byte_size() / 2**20} MiB."
+        )
+        return new_ttn
+
+    def _apply_1q_gate(self, position: int, gate: Op) -> TTN:
+        raise NotImplementedError(
+            "TTN is a base class with no contraction algorithm implemented."
+            + " You must use a subclass of TTN, such as TTNxGate."
+        )
+
+    def _apply_2q_gate(self, positions: tuple[int, int], gate: Op) -> TTN:
+        raise NotImplementedError(
+            "TTN is a base class with no contraction algorithm implemented."
+            + " You must use a subclass of TTN, such as TTNxGate."
+        )
