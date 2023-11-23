439f3752aa0ab9354d7f41d3b205f271cc57a493
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2023-11-23 07:45:56 +0000

    update dependebot and clean up links (#46)

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     author_email="tket-support@cambridgequantum.com",
     python_requires=">=3.9",
     project_urls={
-        "Documentation": "https://cqcl.github.io/pytket-cutensornet/api/index.html",
+        "Documentation": "https://tket.quantinuum.com/extensions/pytket-cutensornet/api/index.html",
         "Source": "https://github.com/CQCL/pytket-cutensornet",
         "Tracker": "https://github.com/CQCL/pytket-cutensornet/issues",
     },
