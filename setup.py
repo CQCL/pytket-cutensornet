e4cf3c69af8e6fda29d3e81669ff7de5ad16843f
Melf <cqc@melf.de>
2023-11-23 18:16:58 +0000

    update pytket version

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     license="Apache 2",
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
-    install_requires=["pytket ~= 1.11"],
+    install_requires=["pytket ~= 1.22"],
     classifiers=[
         "Environment :: Console",
         "Programming Language :: Python :: 3.9",
