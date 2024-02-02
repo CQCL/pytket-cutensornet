4b0ae274c1eb1af5d9f1210c1f5760b18c038e0f
PabloAndresCQ <pablo.andres-martinez@cambridgequantum.com>
2024-02-02 11:34:47 +0000

    Changed kahypar to strict version requirement

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
     license="Apache 2",
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
-    install_requires=["pytket ~= 1.24", "kahypar ~= 1.3.5"],
+    install_requires=["pytket ~= 1.24", "kahypar == 1.3.5"],
     classifiers=[
         "Environment :: Console",
         "Programming Language :: Python :: 3.10",
