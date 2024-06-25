2c5cfc31bbc763f96347174553deb3b214ed851d
PabloAndresCQ <pablo.andresmartinez@quantinuum.com>
2024-06-25 06:37:57 -0700

    Solving circular import

--- pytket/extensions/cutensornet/backends/cutensornet_backend.py
+++ pytket/extensions/cutensornet/backends/cutensornet_backend.py
@@ -23,7 +23,7 @@ from pytket.backends import ResultHandle, CircuitStatus, StatusEnum, CircuitNotR
 from pytket.backends.backend import KwargTypes, Backend, BackendResult
 from pytket.backends.backendinfo import BackendInfo
 from pytket.backends.resulthandle import _ResultIdTuple
-from pytket.extensions.cutensornet import CuTensorNetHandle
+from pytket.extensions.cutensornet.general import CuTensorNetHandle
 from pytket.extensions.cutensornet.general_state import (
     GeneralState,
 )
