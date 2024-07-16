958b03ded48578b348de19891e71e283975ba2e7
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-07-16 11:42:32 +0100

    add navbar config

---
+++ docs/_static/nav-config.js
@@ -0,0 +1,16 @@
+const navConfig = {
+    "navTextLinks": [
+        {
+            "title": string,
+            "href": string,
+        },
+    ],
+    "navProductName": string,
+    "navIconLinks": [
+        {
+            "title": string,
+            "href": string,
+            "iconImageURL": string(i.e. "_static/assets/github.svg"),
+        },
+    ],
+}
