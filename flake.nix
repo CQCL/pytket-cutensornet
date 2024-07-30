07478c8e2230865f9538e5eac586e1c558810c75
Jake Arkinstall <65358059+jake-arkinstall@users.noreply.github.com>
2024-07-30 14:45:52 +0100

    Added nix-community cachix, pinned to tket's nixpkgs, added sympy to mypy ignore list

--- flake.nix
+++ flake.nix
@@ -1,15 +1,15 @@
 {
   description = "Pytket Cutensornet Extension";
-  nixConfig.extra-substituters = "https://tket.cachix.org https://cache.nixos.org https://cuda-maintainers.cachix.org";
+  nixConfig.extra-substituters = "https://tket.cachix.org https://cache.nixos.org https://cuda-maintainers.cachix.org https://nix-community.cachix.org";
   nixConfig.trusted-public-keys = ''
     tket.cachix.org-1:ACdm5Zg19qPL0PpvUwTPPiIx8SEUy+D/uqa9vKJFwh0=
     cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
     cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E=
+    nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
   '';
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.tket.url = "github:CQCL/tket";
-  inputs.tket.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nixpkgs.follows = "tket/nixpkgs";
   outputs = { self, nixpkgs, flake-utils, tket }:
     flake-utils.lib.eachDefaultSystem (system:
       let
