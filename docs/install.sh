de12ff26db024d72caece9b38cc042109f2d046d
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-08-28 18:33:20 +0100

    add some comments

--- docs/install.sh
+++ docs/install.sh
@@ -1,5 +1,8 @@
+# Copy over poetry dependencies from theming repository
 cp pytket-docs-theming/extensions_deps/pyproject.toml .
-cp pytket-docs-theming/extensions_deps/poetry.lock .
+cp pytket-docs-theming/extensio
 
+# Install the docs dependencies. Creates a .venv directory in docs
 poetry install
 
+# Editable wheel should be installed separately.
