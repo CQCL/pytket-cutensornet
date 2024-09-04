caa1fe0a660d7bed7184026686f9bfc10d1e7fd0
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-09-04 01:20:06 +0100

    improve build-docs.sh script

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -2,21 +2,20 @@
 rm -rf build/
 
 # Move theming elements into the docs folder
-mv pytket-docs-theming/_static .
-mv pytket-docs-theming/quantinuum-sphinx .
-mv pytket-docs-theming/conf.py .
+cp -R pytket-docs-theming/_static .
+cp -R pytket-docs-theming/quantinuum-sphinx .
+cp pytket-docs-theming/conf.py .
 
 # Get the name of the project
 EXTENSION_NAME="$(basename "$(dirname `pwd`)")"
 
+# Correct github link in navbar
+sed -i '' 's#CQCL/tket#CQCL/'$EXTENSION_NAME'#' _static/nav-config.js
+
 # Build the docs. Ensure we have the correct project title.
 sphinx-build -b html -D html_title="$EXTENSION_NAME" . build 
 
-# Correct github link in navbar
-sed 's#CQCL/tket#CQCL/'$EXTENSION_NAME'#' _static/nav-config.js
-
-# Move the theming elements back after docs are built. 
-mv _static pytket-docs-theming
-mv quantinuum-sphinx pytket-docs-theming 
-mv conf.py pytket-docs-theming
-# This ensures reusability and doesn't clutter source control.
+# Remove copied files. This ensures reusability.
+rm -r _static 
+rm -r quantinuum-sphinx
+rm conf.py
