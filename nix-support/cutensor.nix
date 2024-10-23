ac23e12187639513a57007aff9dce61bca7f6ba2
Jake Arkinstall <65358059+jake-arkinstall@users.noreply.github.com>
2024-10-23 18:08:30 +0100

    Fixed pycuquantum patch, updated cutensor for cutensornet compat

--- nix-support/cutensor.nix
+++ nix-support/cutensor.nix
@@ -2,17 +2,17 @@
 let
   package_map = {
     x86_64-linux = fetchzip {
-      url = "https://developer.download.nvidia.com/compute/cutensor/redist/libcutensor/linux-x86_64/libcutensor-linux-x86_64-2.0.1.2-archive.tar.xz";
-      sha256 = sha256:OPn5577DBwzcGdSovv5tMv+x3wW2QgF5XVmRgm5y148=;
+      url = "https://developer.download.nvidia.com/compute/cutensor/redist/libcutensor/linux-x86_64/libcutensor-linux-x86_64-2.0.2.5-archive.tar.xz";
+      sha256 = sha256:06ygtOh3potihT0bm0lhytKpf7zWOA8Ap9rATNvQdTk=;
     };
     aarch64-linux = fetchzip {
-      url = "https://developer.download.nvidia.com/compute/cutensor/redist/libcutensor/linux-sbsa/libcutensor-linux-sbsa-2.0.1.2-archive.tar.xz";
-      sha256 = sha256:vTEQ7uJYrEC1Q55DgCO5/PXTm0f34ndqezotnqq6E00=;
+      url = "https://developer.download.nvidia.com/compute/cutensor/redist/libcutensor/linux-sbsa/libcutensor-linux-sbsa-2.0.2.5-archive.tar.xz";
+      sha256 = sha256:CRewkAlXYklDnj6iF6eGv0C8yxHVDOdj+mXqm2dN+po=;
     };
   };
 in pkgs.stdenv.mkDerivation {
   pname = "libcutensor";
-  version = "2.0.1.2";
+  version = "2.0.2.5";
   outputs = [ "out" "lib" "dev" "static" ];
   src = package_map.${pkgs.system};
   installPhase = let
