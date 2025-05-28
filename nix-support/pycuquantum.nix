4a41db24d796cd7feede1538ef3ab0b3ec4bc5df
PabloAndresCQ <pablo.andresmartinez@quantinuum.com>
2025-05-28 18:54:27 +0000

    Sanity recovered?

--- nix-support/pycuquantum.nix
+++ nix-support/pycuquantum.nix
@@ -17,7 +17,7 @@ buildPythonPackage{
     owner = "NVIDIA";
     repo = "cuQuantum";
     rev = "v25.03.0";
-    sha256 = "";
+    sha256 = sha256:TOQX8e2MgefF+FWm0FvAMo7aFlz9S/aPEveeM4eK1Qs=;
   };
   patches = [
     (substituteAll {
